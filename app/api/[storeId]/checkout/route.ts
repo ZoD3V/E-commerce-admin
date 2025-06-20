56b8df4222f1c8252e4a8bf5c8e0615015c56c58
ZoD3V <adzkiyadzikrullah@gmail.com>
2025-06-20 20:36:39 +0700

    update cors

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -6,7 +6,6 @@ var randomstring = require("randomstring");
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 // export async function OPTIONS() {
